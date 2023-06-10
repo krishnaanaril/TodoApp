f10b9517e695a54321c5c39171036e25b16f3ed0
krishnaanaril <krishnamohan.a.m@gmail.com>
2023-06-10 23:23:13 +0530

    chore: add tailwind config

---
+++ src/todo-frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'media',
+  content: [
+    "./projects/todo/src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
