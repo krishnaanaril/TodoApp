ea0c984e66fe75a8af4172238556763b482e25d9
krishnaanaril <krishnamohan.a.m@gmail.com>
2023-06-11 12:25:05 +0530

    feat: did styling for main page

--- src/todo-frontend/projects/todo/src/app/components/main/main.component.ts
+++ src/todo-frontend/projects/todo/src/app/components/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TaskCategoryComponent } from '../task-category/task-category.component';
 
 @Component({
   selector: 'todo-main',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TaskCategoryComponent],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
