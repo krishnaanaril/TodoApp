5fc8edbbaf4d7b9e9ae545a5001880a0ad6717b5
krishnaanaril <krishnamohan.a.m@gmail.com>
2023-06-11 11:42:07 +0530

    feat: add task category component

---
+++ src/todo-frontend/projects/todo/src/app/components/task-category/task-category.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'todo-task-category',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './task-category.component.html',
+  styleUrls: ['./task-category.component.css']
+})
+export class TaskCategoryComponent {
+
+}
