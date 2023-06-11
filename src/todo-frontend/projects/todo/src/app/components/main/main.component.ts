67b6fbc9c0366a685688c7e4b607001ef66db70e
krishnaanaril <krishnamohan.a.m@gmail.com>
2023-06-11 11:41:42 +0530

    feat: add main component

---
+++ src/todo-frontend/projects/todo/src/app/components/main/main.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'todo-main',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent {
+
+}
