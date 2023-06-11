3f2066530c973f1b505660c943e4a0173b699791
krishnaanaril <krishnamohan.a.m@gmail.com>
2023-06-11 11:41:26 +0530

    feat: add route configs

--- src/todo-frontend/projects/todo/src/app/app.routes.ts
+++ src/todo-frontend/projects/todo/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./components/main/main.component')
+            .then(mod => mod.MainComponent)
+    }
+];
